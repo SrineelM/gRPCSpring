341d143fe62d4de04ba0f2fc453c64543fee09eb
Smaz2024 <myapps.smaz@gmail.com>
2025-11-01 11:49:15 +0530

    Corrections to project structure

--- grpc-commn/src/main/java/com/poc/grpc/common/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.poc.grpc.common.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
